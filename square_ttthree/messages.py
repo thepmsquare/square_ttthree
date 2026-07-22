58f901cdc938fbe0e48c8cb7f8194f6de2c82567
Parth Mangtani <impmmmm@gmail.com>
2026-07-22 13:13:07 +0530

    :seedling: add messages

---
+++ square_ttthree/messages.py
@@ -0,0 +1,9 @@
+messages = {
+    "CREATE_SUCCESSFUL": "the record has been created successfully.",
+    "READ_SUCCESSFUL": "the record has been retrieved successfully.",
+    "UPDATE_SUCCESSFUL": "the record has been updated successfully.",
+    "DELETE_SUCCESSFUL": "the record has been deleted successfully.",
+    "GENERIC_204": "no content available for the requested resource.",
+    "GENERIC_400": "the request is invalid or cannot be processed.",
+    "GENERIC_500": "an internal server error occurred. please try again later.",
+}
